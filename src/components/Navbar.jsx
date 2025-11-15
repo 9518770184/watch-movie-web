51f6623859321b2ef8978a36ffb1a4751c8704a0
9518770184 <vijayanddhananjay@gamil.com>
2025-11-15 15:09:40 +0530

    Changes in ui

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,8 +5,8 @@ export const Navbar = () => {
         <>
             <div class="flex border space-x-8 items-center pl-3 py-4">
                 <img className='w-[50px]' src={Logo} alt="No Found" />
-                <a href='/' className='text-blue-500 text-2xl font-bold'>Home</a>
-                <a href='/watchlist' className='text-blue-500 text-2xl font-bold'> WatchList</a>
+                <a href='/' className='text-blue-500 text-x3l font-bold'>Home</a>
+                <a href='/watchlist' className='text-blue-500 text-3xl font-bold'> WatchList</a>
             </div>
         </>
     )
